cb309359e1c3e4ab1a9f0d5ac957728d950ad614
adidner <adidner@gmail.com>
2023-10-11 15:20:42 -0400

    feat: [92739201] we did it, all the pairing stuffs

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -4,4 +4,35 @@ describe('Bowling Game', () => {
   it('starts with score 0', () => {
     expect(new Game().score()).toEqual(0)
   })
+
+  it('updates the score with the value of a roll', () => {
+    const theGame = new Game()
+    theGame.roll(3);
+    expect(theGame.score()).toEqual(3)
+  });
+
+
+  it('counts the next 2 rolls for a strike', () => {
+    const theGame = new Game()
+    theGame.roll(10);
+    theGame.roll(2)
+    theGame.roll(3)
+    expect(theGame.score()).toEqual(20)
+  })
+
+  it('counts two strikes in a row correctly', () => {
+    const theGame = new Game()
+    theGame.roll(10);
+    theGame.roll(10)
+    theGame.roll(3)
+    theGame.roll(2)
+    expect(theGame.score()).toEqual(43)
+  })
+
+  // it('max number rollable is 10', () => {
+  //   const theGame = new Game()
+
+  //   expect(theGame.roll(11)).toThrowError(new Error("Error: roll out of bounds"))
+  // });
+
 })
--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,33 @@
 export class Game {
+  scoreValue:number ;
+  uncalculatedStrikeRollsLeft: number;
+  
+
   constructor() {
-  }
+    this.scoreValue = 0;
+    this.uncalculatedStrikeRollsLeft = 0;
+  }  
 
   roll (pins: number): void {
-
+    // if (pins > 10) {
+    //   throw new Error("Error: roll out of bounds")
+    // }
+    
+    if(this.uncalculatedStrikeRollsLeft > 0){
+      this.scoreValue += (pins * 2)
+      this.uncalculatedStrikeRollsLeft--;
+    }
+    else {
+      this.scoreValue += pins
+    }
+    
+    if(pins == 10){
+      this.uncalculatedStrikeRollsLeft  = 2;
+    }
   }
 
   score (): number {
-    return 0
+    return this.scoreValue
+
   }
 }
