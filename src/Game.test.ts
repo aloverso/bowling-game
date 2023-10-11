8f1cd5493be36122ff8aafc07ef08966e0ef66a3
JP Hechter <jhechter@fearless.tech>
2023-10-11 15:19:15 -0400

    after 4 rounds

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,51 @@
-import { Game } from './Game'
+import { Game } from "./Game";
 
-describe('Bowling Game', () => {
-  it('starts with score 0', () => {
-    expect(new Game().score()).toEqual(0)
-  })
-})
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+describe("Bowling Game", () => {
+  it("starts with score 0", () => {
+    expect(new Game().score()).toEqual(0);
+  });
+
+  it("should have 10 frames", () => {
+    const game = new Game();
+    expect(game.frames.length).toEqual(10);
+  });
+
+  it("starts at frame 0", () => {
+    const game = new Game();
+    expect(game.currentFrame).toEqual(0);
+  });
+
+  it("every 2 rolls the frame should advance", () => {
+    const game = new Game();
+    const firstRoll = getRandomInt(11);
+    game.roll(firstRoll);
+    expect(game.currentFrame).toEqual(0);
+    game.roll(getRandomInt(11 - firstRoll));
+    expect(game.currentFrame).toEqual(1);
+
+    const thirdRoll = getRandomInt(11);
+    game.roll(thirdRoll);
+    game.roll(getRandomInt(11 - thirdRoll));
+    expect(game.currentFrame).toEqual(2);
+  });
+
+  describe("when player has no pending strikes or spares", () => {
+    it("adds the number of pins knocked over to the score", () => {
+      const game = new Game();
+      game.roll(3);
+      expect(game.score()).toEqual(3);
+    });
+  });
+
+  describe("Frames", () => {
+    it("frames 1-9 should have 2 rolls", () => {
+      const game = new Game();
+      const frame = game.frames[getRandomInt(9)];
+      expect(frame.rolls).toEqual(2);
+    });
+  });
+});
--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,37 @@
 export class Game {
+  currentFrame: number;
+  playerScore: number;
+  firstRun: boolean;
+  frames: Frame[] = [];
+
   constructor() {
+    this.playerScore = 0;
+    this.currentFrame = 0;
+    for (let i = 0; i < 10; i++) {
+      this.frames.push(new Frame());
+    }
+    this.firstRun = true;
   }
 
-  roll (pins: number): void {
-
+  roll(pins: number): void {
+    this.playerScore += pins;
+    if (!!this.firstRun) {
+      this.firstRun = false;
+    } else {
+      this.firstRun = true;
+      this.currentFrame += 1;
+    }
   }
 
-  score (): number {
-    return 0
+  score(): number {
+    return this.playerScore;
+  }
+}
+
+export class Frame {
+  rolls: number;
+
+  constructor() {
+    this.rolls = 2;
   }
 }
