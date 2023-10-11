425ec2a5d25c0955af4bcf4fefce671158e1cb2c
Naman Agrawal <naman@innovation.nj.gov>
2023-10-11 15:19:09 -0400

    Code from Namans laptop

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,69 @@
-import { Game } from './Game'
+import { Game } from "./Game";
 
-describe('Bowling Game', () => {
-  it('starts with score 0', () => {
-    expect(new Game().score()).toEqual(0)
-  })
-})
+describe("Bowling Game", () => {
+  it("starts with score 0", () => {
+    expect(new Game().score()).toEqual(0);
+  });
+
+  it("has ten frames", () => {
+    expect(new Game().totalFrames).toEqual(10);
+  });
+
+  it("increments the current frame once per two rolls", () => {
+    const game = new Game();
+
+    expect(game.currentFrame).toEqual(0);
+
+    game.roll(0);
+
+    expect(game.currentFrame).not.toEqual(1);
+
+    game.roll(0);
+
+    expect(game.currentFrame).toEqual(1);
+  });
+
+  it("increments the current frame if the first roll of a frame is 10 pins", () => {
+    const game = new Game();
+    expect(game.currentFrame).toEqual(0);
+
+    game.roll(10);
+    expect(game.currentFrame).toEqual(1);
+  });
+
+  it("increments the current frame if the first roll of a frame is 10 pins", () => {
+    const game = new Game();
+    expect(game.currentFrame).toEqual(0);
+
+    game.roll(0);
+    expect(game.currentFrame).toEqual(0.5);
+
+    game.roll(10);
+    expect(game.currentFrame).toEqual(1);
+  });
+
+  it("increments the score for the frame", () => {
+    const game = new Game();
+    const rollValue: number = 6;
+
+    game.roll(rollValue);
+    game.roll(10 - rollValue);
+    expect(game.score()).toEqual(10);
+  });
+
+  it("adds bonus to score if a spare is rolled", () => {
+    const game = new Game();
+    expect(game.score()).toEqual(0);
+
+    const rollValue = 6;
+    game.roll(rollValue);
+    expect(game.score()).toEqual(6);
+
+    game.roll(10 - rollValue);
+    expect(game.score()).toEqual(10);
+
+    const nextRoll = 2;
+    game.roll(nextRoll);
+    expect(game.score()).toEqual(10 + nextRoll + nextRoll);
+  });
+});
--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,40 @@
 export class Game {
+  totalFrames: number;
+  currentFrame: number;
+  currentScore: number;
+  frameScore: number;
+  applySpareBonus: boolean;
+
   constructor() {
+    this.totalFrames = 10;
+    this.currentFrame = 0;
+    this.currentScore = 0;
+    this.frameScore = 0;
   }
 
-  roll (pins: number): void {
+  roll(pins: number): void {
+    this.currentScore += pins;
+    if (this.applySpareBonus) {
+      this.currentScore += pins;
+      this.applySpareBonus = false;
+    }
 
+    const isFirstRoll = this.currentFrame == Math.floor(this.currentFrame);
+    if (isFirstRoll && pins === 10) {
+      this.currentFrame++;
+      this.frameScore = pins;
+    } else if (!isFirstRoll && pins + this.frameScore === 10) {
+      this.applySpareBonus = true;
+    } else {
+      this.currentFrame += 0.5;
+    }
+
+    if (!isFirstRoll) {
+      this.frameScore = 0;
+    }
   }
 
-  score (): number {
-    return 0
+  score(): number {
+    return this.currentScore;
   }
 }
