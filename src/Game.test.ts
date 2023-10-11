779b77ab64df7a74dc4583895ee1763f4a7e0716
Case Walker <case.b.walker@gmail.com>
2023-10-11 15:18:39 -0400

    Trash

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,38 @@
 import { Game } from './Game'
 
 describe('Bowling Game', () => {
-  it('starts with score 0', () => {
-    expect(new Game().score()).toEqual(0)
+  let game;
+  beforeEach(() => {
+    game = new Game()
   })
+  it('starts with score 0', () => {
+    expect(game.score()).toEqual(0)
+  })
+  it("cannot have a negative score", () => {
+    game.roll(0);
+    expect(game.score()).not.toBeLessThan(0);
+  });
+  it("should have a score equal the number of pins knocked down after one roll", () => {
+    game.roll(7);
+    expect(game.score()).toEqual(7);
+  });
+  it('should add two rolls together', () => {
+    game.roll(6);
+    game.roll(3);
+    expect(game.score()).toEqual(9);
+  });
+  it('If you got a spare, the third score will be 10 + 2*(third)', () => {
+    // frame 1
+    game.roll(6);
+    game.roll(4);
+
+    // frame 2
+    game.roll(8);
+    expect(game.score()).toEqual(26);
+    game.roll(2);
+
+    // frame 3
+    game.roll(3);
+    expect(game.score()).toEqual(34);
+  });
 })
--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,33 @@
+import retryTimes = jest.retryTimes;
+
 export class Game {
+  currentScore: number;
+  rollCount: number
+  frameScore: number
+  prevSpare: boolean
+
   constructor() {
+    this.currentScore = 0;
+    this.rollCount = 0
+    this.frameScore = 0
+    this.prevSpare = false
   }
 
   roll (pins: number): void {
+    this.rollCount += 1;
+    this.frameScore += pins
+    if (this.rollCount % 2 === 0 ) {
 
+      if (this.frameScore === 10) {
+        this.prevSpare = true;
+      }
+      this.frameScore = 0
+    }
+
+    this.currentScore += this.frameScore;
   }
 
   score (): number {
-    return 0
+    return this.currentScore
   }
 }
