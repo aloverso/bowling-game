80965277b6de72f75045fe8a182c142353a47625
Anne LoVerso <anne.loverso@gmail.com>
2023-10-25 19:01:07 -0400

    workshop solo station code

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,7 +1,34 @@
 import { Game } from './Game'
 
 describe('Bowling Game', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game()
+  })
+
   it('starts with score 0', () => {
-    expect(new Game().score()).toEqual(0)
+    expect(game.score()).toEqual(0)
+  })
+
+  it('adds the pins from a roll to the score', () => {
+    game.roll(2)
+    expect(game.score()).toEqual(2)
+    game.roll(3)
+    expect(game.score()).toEqual(5)
+  })
+
+  it('includes the next roll when rolling a spare', () => {
+    game.roll(4)
+    game.roll(6)
+    game.roll(5)
+    expect(game.score()).toEqual(20)
+  })
+
+  it('does not include the next roll when not rolling a spare', () => {
+    game.roll(4)
+    game.roll(5)
+    game.roll(5)
+    expect(game.score()).toEqual(14)
   })
 })
--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,47 @@
 export class Game {
+  runningScore: number;
+  frame: number;
+  ballOfFrame: number;
+  frameScore: number;
+  prevSpare: boolean
+
   constructor() {
+    this.runningScore = 0
   }
 
   roll (pins: number): void {
+    this.setFrame();
+    if (this.ballOfFrame === 0 && this.prevSpare === true) {
+      this.frameScore += pins*2
+      this.prevSpare = false
+    }
+    this.frameScore += pins;
+    if (this.ballOfFrame === 1 && this.frameScore === 10) {
+      this.prevSpare = true
+    }
+    this.runningScore += pins;
+  }
 
+  isSpare (): void {
+    if (this.ballOfFrame == 1 && this.frameScore == 10){
+      this.prevSpare = true
+    }
+    else {
+      this.prevSpare = false
+    }
+  }
+
+  setFrame (): void {
+    if (this.ballOfFrame === 0) {
+      this.ballOfFrame += 1;
+    } else {
+      this.ballOfFrame = 0;
+      this.frame += 1;
+      this.frameScore = 0;
+    }
   }
 
   score (): number {
-    return 0
+    return this.runningScore
   }
 }
